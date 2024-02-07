6b208c0e6fbe3c473b0605589576a9c4cb8bf37f
Alexander4127 <askutsakov@gmail.com>
2024-02-07 13:13:42 +0300

    Fix normalization

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main(cfg: DictConfig):
     train_transforms = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ] +
         ([transforms.RandomHorizontalFlip(), transforms.RandomVerticalFlip()] if cfg.augments.flip else [])
     )
