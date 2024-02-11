One sample test & Fix normalization

main.py
@@ -1,4 +1,5 @@
 import hydra
+import logging
 from omegaconf import DictConfig, OmegaConf
 from pathlib import Path
 import torch
@@ -22,6 +23,7 @@ def main(cfg: DictConfig):
         dict_cfg = OmegaConf.to_container(cfg, resolve=True)
         wandb.init(config=dict_cfg, project=cfg.trainer.wandb.project, name=cfg.trainer.wandb.name)
 
+    logging.info(OmegaConf.to_yaml(cfg))
     ddpm = DiffusionModel(
         eps_model=UnetModel(cfg.arch.in_channels, cfg.arch.out_channels, hidden_size=cfg.arch.hidden_size),
         betas=(cfg.steps.beta_min, cfg.steps.beta_max),
@@ -54,12 +56,13 @@ def main(cfg: DictConfig):
     )
     optim = hydra.utils.instantiate(cfg.optim, ddpm.parameters())
 
+    processing = transforms.Normalize((-1, -1, -1), (2, 2, 2))
     for i in range(cfg.trainer.num_epochs):
         train_epoch(ddpm, dataloader, optim, i, cfg.trainer)
         torch.save(ddpm.state_dict(), f"saved/ch{i}.pth")
-        inits, samples = generate_samples(ddpm, cfg.trainer.device, f"samples/{i:02d}.png")
+        inits, samples = generate_samples(ddpm, cfg.trainer.device, f"samples/{i:02d}", processing)
         if cfg.trainer.wandb.use:
-            wandb.log({"init": wandb.Image(inits), "sample": wandb.Image(samples)}, step=(i + 1) * len(dataloader))
+            wandb.log({"init": inits, "sample": samples}, step=(i + 1) * len(dataloader))
 
 
 if __name__ == "__main__":

modeling/diffusion.py
@@ -32,7 +32,7 @@ class DiffusionModel(nn.Module):
 
         return self.criterion(eps, self.eps_model(x_t, timestep / self.num_timesteps))
 
-    def sample(self, num_samples: int, size, device) -> Tuple[torch.Tensor, torch.Tensor]:
+    def sample(self, num_samples: int, size, device, processing=None) -> Tuple[torch.Tensor, torch.Tensor]:
 
         z_i = torch.randn(num_samples, *size).to(device)
         x_i = z_i.clone()
@@ -43,7 +43,8 @@ class DiffusionModel(nn.Module):
             x_i = self.inv_sqrt_alphas[i].to(device) * (x_i - eps * self.one_minus_alpha_over_prod[i].to(device)) + \
                 self.sqrt_betas[i].to(device) * z
 
-        x_i = transforms.Normalize((-1, -1, -1), (2, 2, 2))(x_i)
+        if processing is not None:
+            x_i = processing(x_i)
         return z_i, x_i
 
 

modeling/training.py
@@ -18,12 +18,7 @@ def train_step(model: DiffusionModel, inputs: torch.Tensor, optimizer: Optimizer
     loss = model(inputs)
     loss.backward()
     optimizer.step()
-    return loss
-
-
-def convert_numpy(samples):
-    grid = make_grid(samples, nrow=4)
-    return grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+    return loss.item()
 
 
 def train_epoch(model: DiffusionModel,
@@ -39,13 +34,21 @@ def train_epoch(model: DiffusionModel,
         loss_ema = train_loss if loss_ema is None else 0.9 * loss_ema + 0.1 * train_loss
         if step % cfg.log_step == 0 and cfg.wandb.use:
             lr = optimizer.param_groups[0]['lr']
-            wandb.log({"loss": train_loss, "lr": lr}, step=n_epoch * len(dataloader) + step)
+            wandb.log(
+                {
+                    "loss": train_loss,
+                    "lr": lr,
+                    "x": wandb.Image(x[0].detach().cpu().numpy())
+                },
+                step=n_epoch * len(dataloader) + step)
         pbar.set_description(f"loss: {loss_ema:.4f}")
 
 
-def generate_samples(model: DiffusionModel, device: str, path: str) -> Tuple[np.ndarray, np.ndarray]:
+def generate_samples(model: DiffusionModel, device: str, path: str, processing=None) -> Tuple[wandb.Image, wandb.Image]:
     model.eval()
     with torch.no_grad():
-        inits, samples = model.sample(8, (3, 32, 32), device=device)
-        save_image(make_grid(samples, nrow=4), path)
-        return convert_numpy(inits), convert_numpy(samples)
+        inits, samples = model.sample(8, (3, 32, 32), device=device, processing=processing)
+        path1, path2 = path + "_init.png", path + "_sample.png"
+        save_image(make_grid(inits, nrow=4), path1)
+        save_image(make_grid(samples, nrow=4), path2)
+        return wandb.Image(path1), wandb.Image(path2)

tests/test_pipeline.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt
+import numpy as np
 from omegaconf import OmegaConf
 import pytest
 import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor, Normalize
 from torchvision.datasets import CIFAR10
+from torchvision.utils import make_grid, save_image
 from tqdm import tqdm
 
 from modeling.diffusion import DiffusionModel
@@ -24,7 +25,7 @@ def train_dataset():
     return dataset
 
 
-@pytest.mark.parametrize(["device"], [["cpu"]])
+@pytest.mark.parametrize(["device"], [["cpu"], ["cuda"]])
 def test_train_on_one_batch(device, train_dataset):
     # note: you should not need to increase the threshold or change the hyperparameters
     ddpm = DiffusionModel(
@@ -35,7 +36,7 @@ def test_train_on_one_batch(device, train_dataset):
     ddpm.to(device)
 
     optim = torch.optim.Adam(ddpm.parameters(), lr=5e-4)
-    dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
 
     x, _ = next(iter(dataloader))
     loss = None
@@ -45,14 +46,31 @@ def test_train_on_one_batch(device, train_dataset):
 
 
 def test_training():
+    class OneSampleDataset(torch.utils.data.Dataset):
+        def __init__(self, parent: torch.utils.data.Dataset, size: int):
+            assert size <= len(parent)
+            self.dataset = parent
+            self.size = size
+            self.idx = np.random.choice(np.arange(len(parent)), size=1, replace=False)
+
+        def __len__(self):
+            return self.size
+
+        def __getitem__(self, item):
+            return self.dataset[self.idx]
+
     # note: implement and test a complete training procedure (including sampling)
-    transforms = Compose([ToTensor(), Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     dataset = CIFAR10(
         "./data",
         train=True,
-        transform=transforms,
+        transform=transforms
     )
+    bs = 2
+    sampled_dataset = OneSampleDataset(dataset, size=2 * bs)
     device = "cpu"
+    samples = torch.concat([sampled_dataset[i][0].unsqueeze(0) for i in range(2 * bs)])
+    save_image(make_grid(samples, nrow=4), "tmp/dataset.png")
 
     ddpm = DiffusionModel(
         eps_model=UnetModel(3, 3, hidden_size=32),
@@ -62,9 +80,11 @@ def test_training():
     ddpm.to(device)
 
     optim = torch.optim.Adam(ddpm.parameters(), lr=5e-4)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    dataloader = DataLoader(sampled_dataset, batch_size=bs, shuffle=True)
 
     cfg = OmegaConf.create({"device": device, "log_step": 10, "wandb": {"use": False}})
-    train_epoch(ddpm, dataloader, optim, 0, cfg)
+    for _ in tqdm(range(1000)):
+        train_epoch(ddpm, dataloader, optim, 0, cfg)
 
-    generate_samples(ddpm, device, "tmp/samples.png")
+    processing = Normalize((-1, -1, -1), (2, 2, 2))
+    generate_samples(ddpm, device, "tmp/samples", processing)